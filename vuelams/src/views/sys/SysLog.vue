3a388cc58ef525818ef2b6e89acad3e0e0cb3eee
huhongjian <294116824@qq.com>
2020-10-20 04:08:48 +0800

    优化个人中心前端格式

--- vuelams/src/views/sys/SysLog.vue
+++ vuelams/src/views/sys/SysLog.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-        操作日志管理
-    </div>
+  <div>
+    操作日志管理
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "SysLog"
-    }
+export default {
+  name: "SysLog"
+}
 </script>
 
 <style scoped>
